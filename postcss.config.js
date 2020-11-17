78e2c96642552b247b9ffc4fb1eb25acbfe8a0d4
hryzi <hryzacek@gmail.com>
2020-11-17 23:22:24 +0100

    Configured PurgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,4 @@
+
 module.exports = {
     plugins: [
       require('tailwindcss'),
@@ -7,7 +8,24 @@ module.exports = {
               './src/**/*.js',
               './src/template.html'
           ],
-          // defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+          defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
       })
     ]
   }
+
+// const purgecss = require('@fullhuman/postcss-purgecss')
+// const cssnano = require('cssnano')
+
+// module.exports = {
+//   plugins: [
+//     require('tailwindcss'),
+//     process.env.NODE_ENV === 'production' ? require('autoprefixer') : null,
+//     process.env.NODE_ENV === 'production'
+//       ? cssnano({ preset: 'default' })
+//       : null,
+//     purgecss({
+//       content: ['./layouts/**/*.html', './src/**/*.vue', './src/**/*.jsx'],
+//       defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+//     })
+//   ]
+// }
